6340b904c47fb4331d3a4569e33c97a3d6fd74e2
andreyd70 <162500429+andreyd70@users.noreply.github.com>
2024-03-17 16:06:02 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-def test():
-    x, y= 4, 6
-    print(x, y)
-test()
+# This is a sample Python script.
 
-def test_2(x,y,z):
-    print(x,y,z)
-test_2(1,2,3)
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
