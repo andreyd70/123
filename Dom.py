f38b001e5d1ff61fc8b17f910bcfba99fe396d36
andreyd70 <162500429+andreyd70@users.noreply.github.com>
2024-03-31 14:43:11 +0300

    Add files via upload

---
+++ Dom.py
@@ -0,0 +1,11 @@
+class House:
+    def __init__(self):
+        self.numberOfFloors = 0
+
+    def setNewNumberOfFloors(self, floors):
+        self.numberOfFloors = floors
+        print(self.numberOfFloors)
+
+
+dom = House()
+dom.setNewNumberOfFloors(floors=67)
