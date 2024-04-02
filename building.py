2baf8ef5bd6569af22c9efbe700de5700740d2da
andreyd70 <162500429+andreyd70@users.noreply.github.com>
2024-04-02 19:01:55 +0300

    Add files via upload

---
+++ building.py
@@ -0,0 +1,15 @@
+class Building:
+    def __init__(self, numberOfFloors, buildingType):
+        self.numberOfFloors = numberOfFloors
+        self.buildingType = buildingType
+    def __eq__(self, other):
+       return self.numberOfFloors == other.numberOfFloors, self.buildingType == other.buildingType
+
+dom1 =Building(numberOfFloors=20,buildingType='brick')
+dom2 =Building(numberOfFloors=5,buildingType='brick')
+print(dom1==dom2)
+
+
+
+
+
