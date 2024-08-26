72c2d7a14e98bc274b5416e84f6d00cf0cddff96
andreyd70 <162500429+andreyd70@users.noreply.github.com>
2024-08-26 11:55:15 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,46 +1,29 @@
-import json
-import os
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 
-allowed_sort_keys = ['firstName', 'lastName', 'department', 'salary']
+app = FastAPI()
 
-def normalize_key(key):
-    return key.lower().replace('_', '')
+# Главная страница
+@app.get("/")
+def read_root():
+    return {"message": "Главная страница"}
 
-def employees_rewrite(sort_type):
-    normalized_sort_type = normalize_key(sort_type)
+# Страница администратора
+@app.get("/user/admin")
+def read_admin():
+    return {"message": "Вы вошли как администратор"}
 
-    if normalized_sort_type not in [normalize_key(k) for k in allowed_sort_keys]:
-        raise ValueError('Bad key for sorting')
+# Страница пользователя по ID
+@app.get("/user/{user_id}")
+def read_user(user_id: int):
+    return {"message": f"Вы вошли как пользователь № {user_id}"}
 
-    if not os.path.exists('employees.json'):
-        raise FileNotFoundError('The file employees.json does not exist.')
+# Страница пользователя с параметрами в строке запроса
+class UserInfo(BaseModel):
+    username: str
+    age: int
 
-    with open('employees.json', 'r') as file:
-        data = json.load(file)
-
-    for employee in data['employees']:
-        for key in list(employee.keys()):
-            normalized_key = normalize_key(key)
-            employee[normalized_key] = employee.pop(key)
-
-    if not all(normalized_sort_type in employee for employee in data['employees']):
-        raise ValueError('Bad key for sorting')
-
-    data['employees'] = sorted(
-        data['employees'],
-        key=lambda x: x[normalized_sort_type],
-        reverse=(normalized_sort_type == 'salary')
-    )
-
-    output_file_name = f'employees_{normalized_sort_type}_sorted.json'
-    with open(output_file_name, 'w') as output_file:
-        json.dump(data, output_file, indent=4)
-
-    print(f'Employees sorted by {normalized_sort_type} and saved to {output_file_name}.')
-
-for key in allowed_sort_keys:
-    try:
-        employees_rewrite(key)
-    except Exception as e:
-        print(f'Error processing {key}: {e}')
+@app.get("/user")
+def read_user_info(username: str, age: int):
+    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
