ced3df3f17c93c4b535b578624d13923e677c807
andreyd70 <162500429+andreyd70@users.noreply.github.com>
2024-08-30 21:19:12 +0300

    Add files via upload

---
+++ module_16_3.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+users = {'1': 'Имя: Example, возраст: 18'}
+
+
+@app.get("/users")
+async def get_users():
+    return users
+
+
+@app.post("/user/{username}/{age}")
+async def create_user(username: str, age: int):
+    user_id = str(max(map(int, users.keys()), default=0) + 1)
+    users[user_id] = f"Имя: {username}, возраст: {age}"
+    return f"User {user_id} is registered"
+
+
+@app.put("/user/{user_id}/{username}/{age}")
+async def update_user(user_id: str, username: str, age: int):
+    if user_id not in users:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    users[user_id] = f"Имя: {username}, возраст: {age}"
+    return f"User {user_id} has been updated"
+
+
+@app.delete("/user/{user_id}")
+async def delete_user(user_id: str):
+    if user_id not in users:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    del users[user_id]
+    return f"User {user_id} has been deleted"
